5ce719e6748911d0d922ab1c61efbd78ff9bbece
Aldhair Vera <aldhairvera15@gmail.com>
2019-09-24 17:44:29 -0500

    Navbar in progress

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import logo from "../images/AldhairVeraIcon.png";
+import './styles/Navbar.css';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div className="Navbar">
+                <div className="Navbar-container">
+                    <div className="Navbar-logo-container">
+                        <img className="Navbar-logo" src={logo} width="80" alt=""/>
+                        <span className="Navbar-title">
+                            <p>
+                                Aldhair
+                                <br/>
+                                Vera
+                                <br/>
+                                Camacho
+                            </p>
+                        </span>
+                    </div>
+                    <nav className="Navbar-links-container">
+                        <ol className="Navbar-link-set">
+                            <li >
+                                <a className="Navbar-links">
+                                    Gamejams
+                                </a>
+                            </li>
+                            <li >
+                                <a className="Navbar-links">
+                                    Own Projects
+                                </a>
+                            </li>
+                            <li >
+                                <a className="Navbar-links">
+                                    Published Projects
+                                </a>
+                            </li>
+                            <li >
+                                <a className="Navbar-links">
+                                    Contact Me
+                                </a>
+                            </li>
+
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
